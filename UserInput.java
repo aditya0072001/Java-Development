c4f406b5ce5dfdaac94e8b059c3a7c1801481c41
aditya0072001 <tripathi.aditya1660@gmail.com>
2019-02-17 13:37:00 +0530

    User Input

---
+++ UserInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UserInput{
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter two numbers to provide there sum");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt(); 
+        int b = input.nextInt(); 
+        int sum=a+b;
+ 
+        System.out.println("Sum is "+sum);
+    }
+}
