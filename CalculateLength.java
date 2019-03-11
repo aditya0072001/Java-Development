93f6ad1a069836b6da1ae6fd4d605f2ccedadc02
aditya0072001 <tripathi.aditya1660@gmail.com>
2019-03-11 12:32:00 +0530

    String length

---
+++ CalculateLength.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CalculateLength{
+        public static void main(String[] args) {
+                Scanner us = new Scanner(System.in);
+                System.out.println("Enter a String :");
+                String str=us.nextLine();
+                int length =str.length();
+                System.out.println("Length is "+length);        
+
+        }
+
+public static void  Decryt(String Hash){
+
+        
+
+        
+        }
+                
+}
