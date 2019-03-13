8b31f74a0ef3a45d413b673cb0e42f8e6f419fac
aditya0072001 <tripathi.aditya1660@gmail.com>
2019-03-13 18:09:46 +0530

    adding

--- CalculateLength.java
+++ CalculateLength.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class CalculateLength{
+
+        public static void  Reverse(String str){
+                String rev = new StringBuffer(str).reverse().toString();
+                System.out.println("String after reverse: "+rev);
+               
+        }      
         public static void main(String[] args) {
                 Scanner us = new Scanner(System.in);
                 System.out.println("Enter a String :");
                 String str=us.nextLine();
                 int length =str.length();
-                System.out.println("Length is "+length);        
+                System.out.println("Length is "+length);
+                Reverse(str);     
 
         }
-
-public static void  Decryt(String Hash){
-
-        
-
-        
-        }
-                
 }
+      
