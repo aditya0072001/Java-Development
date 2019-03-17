bdae521d3e4dd648c774a021ba2dfb018f8d0edd
aditya0072001 <tripathi.aditya1660@gmail.com>
2019-03-17 08:15:53 +0530

    it working again

--- PasswordVerify.java
+++ PasswordVerify.java
@@ -5,22 +5,25 @@ public class PasswordVerify {
 
     public static void main(String[] args) {
         Scanner usr = new Scanner(System.in);
+        boolean compare =false;
+        do{ //Repeats untill compare value is true
         System.out.println("Enter a Password(20 digits long) :");
         String password=usr.nextLine();
         int length=password.length();
-        String[] passworddatabase = {"HelloWorld"};
-        boolean compare =password.equals(passworddatabase);
+        String passworddatabase = new String("HelloWorld");
+        compare =password.equals(passworddatabase); //compare with password database
 
-         if(length < 20 && length > 0) {
+         if(length < 20 && length > 0) { //Length comparision
 
-             if(compare=true)
+             if(compare==true)
                 System.out.println("Password sucessfully verified");
              else
                 System.out.println("Password not verified");
             }
          else{
             System.out.println("Password not of valid length");
-         }  
+         }
+      }while(compare!=true);  
        usr.close();
     }
     
