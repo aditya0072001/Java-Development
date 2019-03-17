d890a7f33a61d440767a8e670c5cb1cf7437bccf
aditya0072001 <tripathi.aditya1660@gmail.com>
2019-03-17 08:06:09 +0530

    i broke the code

--- PasswordVerify.java
+++ PasswordVerify.java
@@ -8,9 +8,15 @@ public class PasswordVerify {
         System.out.println("Enter a Password(20 digits long) :");
         String password=usr.nextLine();
         int length=password.length();
+        String[] passworddatabase = {"HelloWorld"};
+        boolean compare =password.equals(passworddatabase);
 
          if(length < 20 && length > 0) {
+
+             if(compare=true)
                 System.out.println("Password sucessfully verified");
+             else
+                System.out.println("Password not verified");
             }
          else{
             System.out.println("Password not of valid length");
