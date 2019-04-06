120db4e091da425a01dca54ad83ff36bcfdd3185
Ne <41997646+aditya0072001@users.noreply.github.com>
2019-04-06 20:58:49 +0530

    update

--- Data.java
+++ Data.java
@@ -4,22 +4,29 @@ import java.io.IOException;
 
 public class Data {
     public static void main(String[] args) {
-         String Username = "Admin"; //Username for comparision
-         String Temp="a"; //Temp for storing value read from file
+         String Username = "Admin1";
+         String Temp="a";
         FileReader fw;
         {
             try {
-                fw = new FileReader("C:\\Users\\aditya\\IdeaProjects\\Practise\\src\\Username.txt"); //Opening the file
-               
-                for (;fw.read()!=-1; ) {
+                fw = new FileReader("C:\\Users\\aditya\\IdeaProjects\\Practise\\src\\Username.txt");
 
-                        Temp = String.valueOf(fw.read());
-                    }
+                for (; fw.read() != -1; ) {
+
+                    Temp = String.valueOf(fw.read());
+                    System.out.println(Temp);
                 }
+
                 if (Username.equals(Temp)) {
                     System.out.println("Verified");
 
+                }
             } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
