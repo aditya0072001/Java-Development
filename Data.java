23f853f364eba8f1caa73ed26b51dce531f8bfae
Ne <41997646+aditya0072001@users.noreply.github.com>
2019-04-06 21:11:30 +0530

    read

--- Data.java
+++ Data.java
@@ -6,14 +6,15 @@ public class Data {
     public static void main(String[] args) {
          String Username = "Admin1";
          String Temp="";
+         int ch;
         FileReader fw;
         {
             try {
                 fw = new FileReader("C:\\Users\\aditya\\IdeaProjects\\Practise\\src\\Username.txt");
 
-                while(fw.read()!=-1){
+                while((ch=fw.read())!=-1){
 
-                    Temp+= String.valueOf((char)fw.read());
+                    Temp+= String.valueOf((char)ch);
                     System.out.println(Temp);
 
                 }
